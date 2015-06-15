c036daf5a5c706cb7334a6feb99aadf9d0c4bd45
Philip Harris <philip@the-harrises.com>
2015-06-15 17:15:01 +1200

    Init

---
+++ categories.js
@@ -0,0 +1,69 @@
+categories = [
+	{
+		aliases: ["construction","building"],
+		children: [
+			{
+				aliases: ["ladder"]
+			},
+			{
+				aliases: ["fence","fencing"]
+			},
+			{
+				aliases: ["portaloo","loo","toilet"]
+			}
+		]
+	},{
+		aliases: ["catering","event","party","wedding"]
+		children: [
+		    {
+				aliases: ["speaker","sound"]
+			},
+			{
+				aliases: ["tent","hall"]
+			},
+			{
+				aliases: ["dishes","cutlery"]
+				children: [
+					{
+						aliases: ["spoon","teaspoon","tablespoon"]
+					},
+					{
+						aliases: ["plate","platter"]
+					}
+				]
+			},
+			{
+				aliases: ["decoration"],
+				children: [
+					{
+						aliases: ["vase"]
+					},
+					{
+						aliases: ["flower"]
+					}
+				]
+			}
+		]
+	},{
+		aliases: ["travel","tourism"],
+		children: [
+			{
+				aliases: ["bike"]
+			},
+			{
+				aliases: ["car","vehicle"],
+				children: [
+					{
+						aliases: ["campervan","camper","campa"]
+					},
+					{
+						aliases: ["van","truck"]
+					}
+				]
+			},
+			{
+				aliases: ["kayak","boat","jetski"]
+			}
+		]
+	}
+];
