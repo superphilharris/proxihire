783e044c5d5ce06042a45aa0ff10aa3749857fc1
Philip Harris <philip@the-harrises.com>
2015-06-30 06:47:25 +1200

    Move the Rent Stuff All rights reserved

--- public/xmlhttp/searchResults.php
+++ public/xmlhttp/searchResults.php
@@ -3,68 +3,95 @@ $request = array("category"=>"ladder", "latitude"=>null, "longitude"=>null);
 $results = array();
 
 // Construction > Ladder
-echo '<h2>'.$request['category'].'</h2>';
 
 foreach($results as $result){
 	echo '<hr>';
 }
 ?>
-      <h2>Construction > Ladder</h2>
-      
-      <!-- item list -->
-      <div class="panel panel-default">
-        <div class="panel-heading"><a href="">Item heading</a></div>
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-        Aliquam in felis sit amet augue.</p>
-      
-      <hr>
-      
-      <div class="panel panel-default">
-        <div class="panel-heading"><a href="">Item heading</a></div>
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-        Aliquam in felis sit amet augue.</p>
-      
-      <hr>
-      
-      <div class="panel panel-default">
-        <div class="panel-heading"><a href="">Item heading</a></div>
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-        Aliquam in felis sit amet augue.</p>
-      
-      <hr>
-      
-      <div class="panel panel-default">
-        <div class="panel-heading"><a href="">Item heading</a></div>
-      </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-        Aliquam in felis sit amet augue.</p>
-      
-      <hr>
-      <!-- /item list -->
-      
-      <p>
-      <a href="http://www.bootply.com/render/129229">Demo</a> | <a href="http://bootply.com/129229">Source Code</a>
-      </p>
-      
-      <hr> 
-        
-      <p>
-      <a href="http://bootply.com" target="_ext" class="center-block btn btn-primary">More Bootstrap Snippets on Bootply</a>
-      </p>
-        
-      <hr>      
+<ul class="breadcrumb">
+	<li class="dropdown open"><a class="dropdown-toggle" id="branches"
+		role="button" data-toggle="dropdown" href="#">default <b class="caret"></b></a>
+		<ul id="branches-dropdown" class="dropdown-menu">
+			<li><a tabindex="-1" href="...8109a3a986c7">commissions - <code>8109a3a986c7</code></a></li>
+			<li><a tabindex="-1" href="...76d3a7022f9e">dashboard-integration - <code>76d3a7022f9e</code></a></li>
+			<li><a tabindex="-1" href="...4ee00eb42b72">default - <code>4ee00eb42b72</code></a></li>
+			<li><a tabindex="-1" href="...10755b086192">led-support - <code>10755b086192</code></a></li>
+			<li><a tabindex="-1" href="...298fa9e18508">stable - <code>298fa9e18508</code></a></li>
+			<li><a tabindex="-1" href="...990826c5d5df">xdm-customer-selection -
+					<code>990826c5d5df</code>
+			</a></li>
+		</ul> <span class="divider">/</span></li>
+	<li><a href="...4ee00eb42b72">aeroclub.aeroexpress</a> <span
+		class="divider">/</span></li>
+</ul>
 
-    </div>
-    
-    
+<!-- item list -->
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<a href="">Item heading</a>
+	</div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+	pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie
+	tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+	dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis.
+	Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</p>
+
+<hr>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<a href="">Item heading</a>
+	</div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+	pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie
+	tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+	dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis.
+	Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</p>
+
+<hr>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<a href="">Item heading</a>
+	</div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+	pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie
+	tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+	dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis.
+	Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</p>
+
+<hr>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<a href="">Item heading</a>
+	</div>
+</div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+	pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie
+	tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+	dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis.
+	Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</p>
+
+<hr>
+<!-- /item list -->
+
+<p>
+	<a href="http://www.bootply.com/render/129229">Demo</a> | <a
+		href="http://bootply.com/129229">Source Code</a>
+</p>
+
+<hr>
+
+<p>
+	<a href="http://bootply.com" target="_ext"
+		class="center-block btn btn-primary">More Bootstrap Snippets on
+		Bootply</a>
+</p>
+
+<hr>
+
+
