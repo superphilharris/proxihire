40493aec7b909dbec559a38c083f7ddb9ac7f9ef
Jonathan Harris <jonathan@the-harrises.com>
2015-07-10 08:21:10 +1200

    Added database versioning

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,6 +2,7 @@
 
 APACHE_CONFIG=${APACHE_CONFIG:-/etc/apache2/apache2.conf}
 APACHE_SITE_CONFIG=${APACHE_SITE_CONFIG:-/etc/apache2/sites-available/rentstuff.conf}
+APACHE_PHP_CONFIG=${APACHE_PHP_CONFIG:-/etc/php5/apache2/php.ini}
 
 # If it is installed, use meld as the default diff viewer
 if which meld > /dev/null; then
@@ -70,7 +71,7 @@ function install_pkg {
 				if confirm "Install it?" y; then
 					if ! sudo apt-get install $PACKAGE; then
 						echo "Could not install $PACKAGE."
-						exit 1
+						return 1
 					fi
 				fi
 			fi
@@ -80,7 +81,7 @@ function install_pkg {
 				#echo "$PACKAGE isn't installed."
 				#if ! sudo npm install "$PACKAGE"; then
 					#echo "Could not install $PACKAGE."
-					#exit 1
+					#return 1
 				#fi
 			#fi
 			#;;
@@ -89,13 +90,13 @@ function install_pkg {
 				echo "$PACKAGE isn't installed."
 				if ! php composer.phar require "$PACKAGE"; then
 					echo "Could not install $PACKAGE"
-					exit 1
+					return 1
 				fi
 			fi
 			;;
 		*)
 			echo "Unknown package manager $MANAGER"
-			exit 1
+			return 1
 			;;
 	esac
 }
@@ -127,7 +128,7 @@ function create_symbolic_link {
 				return 1
 			fi
 			echo "Backing up ${NAME} to ${NAME}.bak"
-			sudo mv "${NAME}" "${NAME}.bak" || exit 1
+			sudo mv "${NAME}" "${NAME}.bak" || return 1
 		else
 			echo "Configuration file does not exist at $NAME."
 			if ! confirm "Do you wish to create it as a link to $TARGET?" y; then
@@ -171,19 +172,23 @@ if ! php composer.phar > /dev/null; then
 fi
 
 ## Install npm
-#install_pkg npm || exit
+#install_pkg npm || exit 1
 
 # ## Install other dependencies
 
 # install vim for vimdiff (used in this setup script).
-install_pkg vim || exit
+install_pkg vim || exit 1
 
 # core web services
-install_pkg apache2 || exit
-install_pkg php5 || exit
+install_pkg apache2 || exit 1
+install_pkg php5 || exit 1
+install_pkg php5-mysql || exit 1
 
 # ZFTool
-install_pkg -m composer zendframework/zftool:dev-master || exit
+install_pkg -m composer zendframework/zftool:dev-master || exit 1
+
+# mysql
+install_pkg mysql-server || exit 1
 
 #-------------------------------------------------------------------------------
 # # APACHE SET-UP
@@ -191,8 +196,9 @@ install_pkg -m composer zendframework/zftool:dev-master || exit
 sudo a2enmod php5
 sudo a2enmod rewrite
 
-check_config "$APACHE_CONFIG" || exit
-check_config "$APACHE_SITE_CONFIG" || exit
+check_config "$APACHE_CONFIG" || exit 1
+check_config "$APACHE_SITE_CONFIG" || exit 1
+check_config "$APACHE_PHP_CONFIG" || exit 1
 
 create_symbolic_link "${PWD}/public" "/var/www"
 
@@ -206,3 +212,53 @@ sudo a2ensite rentstuff.conf
 
 # reload apache
 sudo service apache2 reload
+
+#-------------------------------------------------------------------------------
+# # MYSQL SET-UP
+
+source ${SCRIPT_DIR}/get_mysql_password_option root MYSQL_ROOT_PASSOPT
+# check the root password
+mysql -u root $MYSQL_ROOT_PASSOPT <<< "quit" || exit 1
+
+source ${SCRIPT_DIR}/get_mysql_password_option apache MYSQL_APACHE_PASSOPT
+[ -n "$MYSQL_APACHE_PASSOPT" ] && \
+    mysql_apache_password="${MYSQL_APACHE_PASSOPT// -p/}"
+
+# see if the database `RentStuff` is created
+if ! mysql -u root $MYSQL_ROOT_PASSOPT RentStuff <<< "quit" > /dev/null 2>&1; then
+	[ -n "$MYSQL_APACHE_PASSOPT" ] && \
+		mysql_apache_identified_by=" IDENTIFIED BY '${MYSQL_APACHE_PASSOPT// -p/}'"
+
+	# Create the apache user
+	mysql -u root $MYSQL_ROOT_PASSOPT <<< \
+"CREATE USER 'apache'@'localhost'${mysql_apache_identified_by};"
+
+	# Create the rentstuff database
+	mysql -u root $MYSQL_ROOT_PASSOPT <<< \
+"CREATE DATABASE RentStuff;
+GRANT SELECT, DELETE, UPDATE, INSERT ON RentStuff.* TO 'apache'@'localhost';" || exit 1
+
+fi
+
+# check the apache password
+mysql -u apache $MYSQL_APACHE_PASSOPT <<< "quit" || exit 1
+
+#-------------------------------------------------------------------------------
+# # DBV SET-UP
+
+MYSQL_APACHE_PASSWORD=${MYSQL_APACHE_PASSOPT// -p/}
+while ! grep -F "define('DB_PASSWORD', '$MYSQL_APACHE_PASSWORD');" "public/dbv/config.php" > /dev/null 2>&1;
+do
+	if [ ! -e "public/dbv/config.php" ]
+	then
+		echo "Setting up database versioning. This can be accessed at 'http://<servername>/dbv'."
+		echo "You will need to enter the MYSQL root password into the dbv config file."
+		cp public/dbv/config.php.sample public/dbv/config.php
+	else
+		echo "It appears as though the apache password in the dbv config file is incorrect."
+	fi
+	read -p "Press [enter] to config it." -s
+	vim -c ":set hlsearch" -c "/'DB_PASSWORD', '[^']*'" public/dbv/config.php
+	echo
+	echo
+done
