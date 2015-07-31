54431746b6a65cb7e3094658369e900c36372e51
Philip Harris <philip@the-harrises.com>
2015-08-01 08:21:53 +1200

    Removed duplicate maps from main page

--- public/js/index.js
+++ public/js/index.js
@@ -16,29 +16,25 @@ $(document).ready(function(){
 		}
 		return returnList;
 	}
-	console.log(linearize(categories));
 	
 	var substringMatcher = function(strs) {
-		  return function findMatches(q, cb) {
-		    var matches, substringRegex;
-		 
-		    // an array that will be populated with substring matches
-		    matches = [];
-		 
-		    // regex used to determine if a string contains the substring `q`
-		    substrRegex = new RegExp(q, 'i');
-		 
-		    // iterate through the pool of strings and for any string that
-		    // contains the substring `q`, add it to the `matches` array
-		    $.each(strs, function(i, str) {
-		      if (substrRegex.test(str)) {
-		        matches.push(str);
-		      }
-		    });
-		 
-		    cb(matches);
-		  };
-		};
+	  return function findMatches(q, cb) {
+	    var matches, substringRegex;
+	    // an array that will be populated with substring matches
+	    matches = [];
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        matches.push(str);
+	      }
+	    });
+	    if(q.toLowerCase() == matches[0]) console.log('typed the only suggestion: '+matches[0]);
+	    cb(matches);
+	  };
+	};
 	
 	$('#mainSearchBar').typeahead({
 		hint: true,
@@ -47,6 +43,12 @@ $(document).ready(function(){
 	}, {
 		source: substringMatcher(linearize(categories)),
 		name: 'categories'
-	})
-	// TODO psh send list to typeahead search box.
+	}).bind('typeahead:select', function(ev, suggestion){
+		window.location.href = suggestion;
+	}).bind('typeahead:autocomplete', function(ev, suggestion){
+		$('#mainSearchBar').blur();
+		window.location.href = suggestion;
+	}).bind('typeahead:cursorchange', function(ev, suggestion){
+		console.log('todo: could have async fetching here?'+suggestion);
+	});
 });
