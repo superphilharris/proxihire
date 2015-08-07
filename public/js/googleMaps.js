d6bbc21fa35fa3f6921d005a6867e3c405a5be11
Philip Harris <philip@the-harrises.com>
2015-08-07 22:03:58 +1200

    Do no display google maps for phone screens

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,25 +1,29 @@
 
-var allMarkers = [];
+var showGoogleMap = (window.innerWidth > 768);
+var allMarkers = [], allAssets = [];
 var googleMap = null, mapsBouncingTimout = null;
-function addMapsMarker(lat, long, title, url){
-	allMarkers.push(new google.maps.Marker({
-		position: 	new google.maps.LatLng(lat, long),
-		url: 		url,
-		title: 		title
-	}));
-}
 function showAllMarkers(){
+	if(googleMap == null) return;
+	for(var i in allAssets){
+		allMarkers.push(new google.maps.Marker({
+			position: 	new google.maps.LatLng(allAssets[i].lat, allAssets[i].long),
+			url: 		allAssets[i].url,
+			title: 		allAssets[i].id + ''
+		}));
+	}
 	for(var i=0; i<allMarkers.length; i++) {
 		allMarkers[i].setMap(googleMap);
 	}
 }
 function removeAllMarkers(){
+	if(googleMap == null) return;
 	for(var i=0; i<allMarkers.length; i++) {
 		allMarkers[i].setMap(null);
 	}
 	allMarkers = [];
 }
 function bounceMarker(url){
+	if(googleMap == null) return;
 	for(var i=0; i<allMarkers.length; i++) {
 		if(url == allMarkers[i].url) {
 			allMarkers[i].setAnimation(google.maps.Animation.BOUNCE);
@@ -30,6 +34,7 @@ function bounceMarker(url){
 	}
 }
 function stopBouncing(url, seconds){
+	if(googleMap == null) return;
 	clearTimeout(mapsBouncingTimout);
 	mapsBouncingTimout = setTimeout(function(){
 		for(var i=0; i<allMarkers.length; i++) {
@@ -40,24 +45,21 @@ function stopBouncing(url, seconds){
 	}, seconds * 1000)
 }
 
-$(document).ready(function(){/* google maps -----------------------------------------------------*/
-	google.maps.event.addDomListener(window, 'load', initialize);
-	
-	
-	function initialize() {
-	
-	  /* position Amsterdam */
-	  var latlng = new google.maps.LatLng(-36.84913134182603, 174.76234048604965);
-	
-	  var mapOptions = {
-	    center: latlng,
-	    scrollWheel: false,
-	    mapTypeControl: false,
-	    zoom: 13
-	  };
+function initializeGoogleMaps() {
+	google.maps.event.addDomListener(window, 'load', function(){
+		var latlng = new google.maps.LatLng(-36.84913134182603, 174.76234048604965);
+		var mapOptions = {
+	    	center: latlng,
+	    	scrollWheel: false,
+	    	mapTypeControl: false,
+	    	zoom: 13
+	  	};
 	  
-	  googleMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-	  showAllMarkers();
-	};
-	/* end google maps -----------------------------------------------------*/
-});
+	  	googleMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	  	showAllMarkers();
+	});
+};
+
+if(showGoogleMap){
+	$.getScript('http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed&callback=initializeGoogleMaps');
+}
