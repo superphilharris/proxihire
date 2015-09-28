69fd3e86d6f43ca455c25bd6c19d0c7fc78d5d11
Jonathan Harris <jonathan@the-harrises.com>
2015-09-29 06:14:50 +1300

    Made headings in importer output MYSQL comments

--- module/ScraperImporter/src/ScraperImporter/Service/ImporterService.php
+++ module/ScraperImporter/src/ScraperImporter/Service/ImporterService.php
@@ -76,7 +76,7 @@ class ImporterService implements ImporterServiceInterface
 		$categories = $this->helper->jsonDecode($categories_json);
 		
 		// 2. Read in the crawled lessors
-		echo '<h1>Create Lessor SQL:</h1><code>';
+		echo '<h1>-- Create Lessor SQL:</h1><code>';
 		$createdLessors = array();
 		foreach($pages as $lessor){
 			if($lessor->item_type === "lessor"){
@@ -96,7 +96,7 @@ class ImporterService implements ImporterServiceInterface
 		}
 		
 		// 3. Delete any existing assets
-		echo '</code><h1>Delete Existing Assets SQL:</h1><code>';
+		echo '</code><h1>-- Delete Existing Assets SQL:</h1><code>';
 		$createdLessors = array();
 		foreach($pages as $lessor){
 			if($lessor->item_type === "lessor"){
@@ -109,7 +109,7 @@ class ImporterService implements ImporterServiceInterface
 		}
 		
 		// 4. Update any missing categories
-		echo '</code><h1><span style="color: white;">-- </span>New Assets SQL:</h1><code>';
+		echo '</code><h1><span style="color: white;">-- </span>-- New Assets SQL:</h1><code>';
 		if(true){ // Create new categories and datatypes
 			exec('php '.__DIR__.'/../../../../../tools/generate_category_sql.php > /tmp/.tmp_category.sql');
 			$sql = file_get_contents('/tmp/.tmp_category.sql');
