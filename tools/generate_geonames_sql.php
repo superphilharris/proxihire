6800f19d45dda92c2c2a163d239a883ae47f4e85
Jonathan Harris <jonathan@the-harrises.com>
2015-10-31 07:34:19 +1300

    Updated geoname generation for mysql sanitizing.

--- tools/generate_geonames_sql.php
+++ tools/generate_geonames_sql.php
@@ -1,7 +1,12 @@
 <?php
 
+$dbConfig=require( dirname(__FILE__)."/../config/autoload/db.local.php");
+$dbConfig=$dbConfig['db'];
+mysql_connect( 'localhost', $dbConfig['username'], $dbConfig['password'] );
+
 $country = "NZ";
 generateSQL($country);
+
 
 function generateSQL($country){
 	$filename = dirname(__FILE__)."/geonames/".$country.".txt";
@@ -10,10 +15,13 @@ function generateSQL($country){
 		while(!feof($fh)){
 			$line = fgets($fh);
 			$columns = explode("\t", $line);
+			foreach( $columns as $key => &$column ){
+				$columns[$key] = mysql_real_escape_string($column);
+			}
 			if(count($columns) > 8){
 				echo "INSERT IGNORE INTO geoname (geoname_id, name_fulnam, latitude_float, longitude_float, country_code) VALUES ('".
 						$columns[0]."','".
-		   				stripslashes($columns[2])."','".
+						$columns[2]."','".
 						$columns[4]."','".
 						$columns[5]."','".
 						$columns[8]."'); \n";
